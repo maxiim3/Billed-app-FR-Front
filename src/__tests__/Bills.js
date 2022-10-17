9fb35072fa1bf93eea86f1ae5b1275f1c3d877db
Maxime Tamburrini <maximemickael.contact@gmail.com>
2022-10-17 15:43:27 +0200

    fix test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,8 +4,8 @@
 
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import {bills} from "../fixtures/bills.js"
+import {ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
@@ -14,10 +14,10 @@ describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      Object.defineProperty(window, 'localStorage', {value: localStorageMock})
       window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
+                                                           type: 'Employee'
+                                                         }))
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
@@ -26,14 +26,24 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
     })
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
+           document.body.innerHTML = BillsUI({data: bills})
+           let regExp = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i;
+           const dates = screen.getAllByText(regExp).map(a => a.innerHTML)
+           const datesSorted = dates.sort(antiChrono)
+           // const datesSorted = [...dates].sort(antiChrono) // NOTE : dates n'est pas modifier, donc ne sera jamais trier dans l'ordre et ne correspondra jamais au résultat modifier...
+           expect(dates).toEqual(datesSorted)
+           // expect(dates).toEqual(datesSorted)
+         }
+    )
   })
 })
+
+const antiChrono = (a, b) => {
+  const [year_a, month_a, day_a] = a.split('-')
+  const [year_b, month_b, day_b] = b.split('-')
+  if (year_a !== year_b) return year_b - year_a
+  if (month_a !== month_b) return month_b - month_a
+  return day_b - day_a
+}
