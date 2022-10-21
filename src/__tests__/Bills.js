64239ad8c9020e71154f20413404cad200b33b06
Maxime Tamburrini <maximemickael.contact@gmail.com>
2022-10-21 10:18:16 +0200

    fix test in bills.js

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -11,39 +11,40 @@ import {localStorageMock} from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
+    describe("When I am on Bills Page", () => {
+        it("Should highlight the window icon", async () => {
 
-      Object.defineProperty(window, 'localStorage', {value: localStorageMock})
-      window.localStorage.setItem('user', JSON.stringify({
-                                                           type: 'Employee'
-                                                         }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
+            Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+            window.localStorage.setItem('user', JSON.stringify({
+                                                                   type: "Employee",
+                                                                   password: "employee",
+                                                                   email: "employee@test.tld",
+                                                                   status: "connected"
+                                                               }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.append(root)
+            router()
+            window.onNavigate(ROUTES_PATH.Bills)
+            await waitFor(() => screen.getByTestId('icon-window'))
+            const windowIcon = screen.getByTestId('icon-window')
+            expect(windowIcon.className).toBe("active-icon")
+        })
+        it("Should ordered order the bills from earliest to latest", () => {
+                 document.body.innerHTML = BillsUI({data: bills})
+                 let regExp = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i;
+                 const antiChrono = (a, b) => {
+                     const [year_a, month_a, day_a] = a.split('-')
+                     const [year_b, month_b, day_b] = b.split('-')
+                     if (year_a !== year_b) return year_b - year_a
+                     if (month_a !== month_b) return month_b - month_a
+                     return day_b - day_a
+                 }
+                 const dates = screen.getAllByText(regExp).map(a => a.innerHTML)
+                 const datesSorted = dates.sort(antiChrono)
+                 expect(dates).toEqual(datesSorted)
+             }
+        )
     })
-    test("Then bills should be ordered from earliest to latest", () => {
-           document.body.innerHTML = BillsUI({data: bills})
-           let regExp = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i;
-           const dates = screen.getAllByText(regExp).map(a => a.innerHTML)
-           const datesSorted = dates.sort(antiChrono)
-           // const datesSorted = [...dates].sort(antiChrono) // NOTE : dates n'est pas modifier, donc ne sera jamais trier dans l'ordre et ne correspondra jamais au résultat modifier...
-           expect(dates).toEqual(datesSorted)
-           // expect(dates).toEqual(datesSorted)
-         }
-    )
-  })
 })
 
-const antiChrono = (a, b) => {
-  const [year_a, month_a, day_a] = a.split('-')
-  const [year_b, month_b, day_b] = b.split('-')
-  if (year_a !== year_b) return year_b - year_a
-  if (month_a !== month_b) return month_b - month_a
-  return day_b - day_a
-}
